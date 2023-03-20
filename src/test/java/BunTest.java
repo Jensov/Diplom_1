06e007633f4327ce37d5fe138f3a20eeed26f369
Ivan Cherepanov <jensoff@yandex.ru>
2023-03-20 20:52:11 +0400

    Фикс после ревью : Добавил параметризацию и передаю с параметрами 1) Обычные значения 2) Пустая строка и 0 3) Длинное название и минимальное положительное значение 4) Спецсимволы и отрицательное значение 5) Null и максимальное положительное значение

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,18 +1,36 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
-
-    private final String name = "БУЛОЧКА С КУНЖУТОМ";
-    private final float price = 300;
-    Bun bun = new Bun(name, price);
+    private String name;
+    private float price;
 
     public final static Double DELTA = 0.00001;
 
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunData() {
+        return new Object[][]{
+                {"Булочка с кунжутом", 300},
+                {"", 0},
+                {"Булочка с кунжутомБулочка с кунжутомБулочка с кунжутомБулочка с кунжутом", 0.0000001F},
+                {"!#**&$!)*$_!($+!((!($&*!^$", -1},
+                {null, 9999999999.99F}
+        };
+    }
+
     @Test
     public void setBunName() { // ТЕСТ ИМЕНИ БУЛКИ
+        Bun bun = new Bun(name, price);
         String expected = name;
         String actual = bun.getName();
         assertEquals(expected, actual);
@@ -20,6 +38,7 @@ public class BunTest {
 
     @Test
     public void setBunPrice() { // ТЕСТ ЦЕНЫ БУЛКИ
+        Bun bun = new Bun(name, price);
         float expected = price;
         float actual = bun.getPrice();
         assertEquals(expected, actual, DELTA);
